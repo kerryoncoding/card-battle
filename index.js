ca498e79f4c77a679521a5a4a1e3a25c5e775449
kerryoncoding <103139429+kerryoncoding@users.noreply.github.com>
2022-09-17 08:28:59 -0400

    connected to axios.  Made api call

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 
 
 function shuffle() {
+    let apiShuffle = 'http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
     alert("shuffle");
+    axios.get(apiShuffle).then(displayDeckId);
+};
+
+function displayDeckId(response) {
+    remaining_cards = response.data.remaining;
+    console.log(remaining_cards);
+    deck_name=response.data.deck_id;
+    console.log(deck_name);
 }
 
 function playCard() {
     alert("next Card");
 }
+
+let deck_name = null;
+let remaining_cards= null;
 
 document.getElementById("start").addEventListener ("click", shuffle);
 
