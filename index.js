eea2d888628a82f675093b6ca8d5eea10aa5955e
kerryoncoding <103139429+kerryoncoding@users.noreply.github.com>
2023-02-12 13:22:55 -0500

    styling.  Adding visual cues

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
-
 function shuffle() {
     let apiShuffle = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
     axios.get(apiShuffle).then(displayDeckId);
-    document.querySelector("#button").innerHTML=`<button class="btn btn-secondary" id="play">Play Cards</button>`;
+    document.querySelector("#button").innerHTML=`<button class="btn btn-secondary" id="play">Draw Cards</button>`;
     document.getElementById("play").addEventListener ("click", playCard);
 };
 
@@ -22,7 +21,6 @@ function playCard() {
 
 
 function battle(cards) {
-
     let cardsRemaining = document.querySelector("#card-count");
     cardsRemaining.innerHTML = cards.data.remaining;
     let computerCard= document.querySelector("#computer-card");
@@ -30,7 +28,7 @@ function battle(cards) {
     let playerCard= document.querySelector("#player-card");
     playerCard.innerHTML = `<img src=${cards.data.cards[1].image} width="85%" id="player-card">`;
     console.log(computer_value);
-            
+    
     computer_value = (cards.data.cards[0].value);   
     if (computer_value === `JACK`) {
         computer_value = "11";
@@ -61,16 +59,16 @@ function battle(cards) {
 
     if (Number(computer_value) > Number(player_value)) {
         computer_score = computer_score + 2;
-        document.querySelector("#computer-score").innerHTML = `<span>${computer_score}</span>`;
+        document.querySelector("#computer-score").innerHTML = `<span style="color:goldenRod">${computer_score}</span>`;
         document.querySelector("#player-score").innerHTML = `<span style="color:white">${player_score}</style>`;
-        playerCard.innerHTML = `<img src=${cards.data.cards[1].image} width="85%" id="player-card" style="border: 20px solid green; border-radius:10px">`;
+        playerCard.innerHTML = `<img src=${cards.data.cards[1].image} width="85%" id="player-card">`;
         
     } else { 
             if (Number(computer_value) < Number(player_value)){
             player_score = player_score + 2;
             document.querySelector("#player-score").innerHTML = `<span>${player_score}</style>`;
             document.querySelector("#computer-score").innerHTML = `<span style="color:white">${computer_score}</span>`;
-            computerCard.innerHTML = `<img src=${cards.data.cards[0].image} width="85%" id="computer-card" style="border: 20px solid green; border-radius:10px">`;
+            computerCard.innerHTML = `<img src=${cards.data.cards[0].image} width="85%" id="computer-card">`;
             } else {
                 computer_score = computer_score + 1;
                 player_score = player_score + 1;
